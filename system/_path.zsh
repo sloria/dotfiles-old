3138569db0b1b3acd4832d2921a90f2131df0676
sloria <sloria1@gmail.com>
2014-05-15 22:06:49 -0400

    Fix bin

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,4 +1,4 @@
 # Customize to your needs...
-export PATH=./bin:/usr/local/share/npm/lib/node_modules/karma/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/texbin
+export PATH=$ZSH/bin:/usr/local/share/npm/lib/node_modules/karma/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/texbin
 export PATH=/usr/local/share/npm/bin:$PATH
 
