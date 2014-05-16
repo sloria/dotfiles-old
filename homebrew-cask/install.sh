84e712f811b3c435807f09ff06e761da22e9cc3d
sloria <sloria1@gmail.com>
2014-05-15 23:42:28 -0400

    Clean up

--- homebrew-cask/install.sh
+++ homebrew-cask/install.sh
@@ -4,17 +4,18 @@
 #
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew cask
+
 # Make sure brew cask is installed
 brew tap caskroom/cask
 brew install brew-cask
 
-alias cask="brew cask"
+echo "Installing brew cask dependencies"
 
-cask install google-chrome
-cask install dropbox
-cask install iterm2
-cask install flux
-cask install bettertouchtool
-cask install crashplan
+brew cask install google-chrome
+brew cask install dropbox
+brew cask install iterm2
+brew cask install flux
+brew cask install bettertouchtool
+brew cask install crashplan
 
 echo "Installed cask dependencies"
