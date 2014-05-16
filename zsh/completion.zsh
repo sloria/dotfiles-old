80e7286452dae2bfb8314e11f21b0bf95a4d9572
sloria <sloria1@gmail.com>
2014-05-16 01:41:34 -0400

    Use preszto for zsh configuration

--- zsh/completion.zsh
+++
@@ -1,8 +0,0 @@
-# matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
-# pasting with tabs doesn't perform completion
-zstyle ':completion:*' insert-tab pending
-
-
-fpath=(/usr/local/share/zsh-completions $fpath)
