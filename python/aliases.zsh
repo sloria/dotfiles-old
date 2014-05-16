0901b6511d023aced816c9ebb331b040c41f4493
sloria <sloria1@gmail.com>
2014-05-16 02:21:00 -0400

    Add autoenv

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,7 +1,14 @@
 alias py="python"
 alias py3="python3"
+alias ipy="ipython"
 alias pyserv="python -m SimpleHTTPServer"
 alias pyserv3="python3 -m http.server"
 alias pt="pytest"
 alias nt="nosetests"
 alias mn="python manage.py"
+
+# Conda environments
+alias createenv="conda create python=2 ipython pip -n "
+alias createenv3="conda create python=3 ipython pip -n "
+alias wo="source activate"
+alias de="source deactivate"
