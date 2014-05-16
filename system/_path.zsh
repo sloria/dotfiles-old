f121d8102dccb34255b01457c1206431256853c1
sloria <sloria1@gmail.com>
2014-05-16 18:47:29 -0400

    Fix path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,4 +1,4 @@
 # Customize to your needs...
-export PATH=$ZSH/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/texbin
+export PATH=$ZSH/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/texbin:$PATH
 
 
