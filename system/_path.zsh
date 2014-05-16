8b94cec16db79fd90df94940d7ad6b50926613e2
sloria <sloria1@gmail.com>
2014-05-16 01:09:15 -0400

    Include miniconda in PATH

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,4 +1,6 @@
 # Customize to your needs...
 export PATH=$ZSH/bin:/usr/local/share/npm/lib/node_modules/karma/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/texbin
 export PATH=/usr/local/share/npm/bin:$PATH
+# If you use anaconda python (miniconda distribution)
+export PATH=~/miniconda/bin:$PATH
 
