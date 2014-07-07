2a8c397014b16af0fd20ab4dd2e830eeda216b51
sloria <sloria1@gmail.com>
2014-07-06 21:27:39 -0400

    vimrc.after customizations

--- vim/install.sh
+++ vim/install.sh
@@ -2,3 +2,8 @@ brew install macvim
 # Install the janus vim distribution
 curl -Lo- https://bit.ly/janus-bootstrap | bash
 
+# Make sure that .janus directory exists
+if [ -d "$HOME/.janus"]; then
+  mkdir $HOME/.janus
+fi
+
