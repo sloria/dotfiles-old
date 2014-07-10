75493a95b19694886d64f789a48bf7a1d25ec849
sloria <sloria1@gmail.com>
2014-07-10 00:01:31 -0400

    Only run homebrew install on mac

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -4,20 +4,23 @@
 #
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
-
-# Check for Homebrew
-if test ! $(which brew)
+unamestr=`uname -a`
+if [[ "$unamestr" == *Darwin* ]]
 then
-  echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" > /tmp/homebrew-install.log
+  # Check for Homebrew
+  if test ! $(which brew)
+  then
+    echo "  Installing Homebrew for you."
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" > /tmp/homebrew-install.log
+  fi
+
+  # Link the Brewfile
+  if [[ ! -a ~/Brewfile ]]; then
+      ln -s $ZSH/homebrew/Brewfile ~/Brewfile
+  fi
+
+  # install dependencies in Brewfile
+  brew bundle ~/Brewfile
+
+  exit 0
 fi
-
-# Link the Brewfile
-if [[ ! -a ~/Brewfile ]]; then
-    ln -s $ZSH/homebrew/Brewfile ~/Brewfile
-fi
-
-# install dependencies in Brewfile
-brew bundle ~/Brewfile
-
-exit 0
--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,9 @@
+#!/usr/bin/env zsh
 # Install the janus vim distribution
 curl -Lo- https://bit.ly/janus-bootstrap | bash
 
 # Make sure that .janus directory exists
-if [[ ! -a $HOME/.janus ]]; then
+if [[ ! -a $HOME/.janus ]]
+then
     ln -s $ZSH/vim/janus $HOME/.janus
 fi
