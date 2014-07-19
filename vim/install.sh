8996aa7e091ffef8f194b537b34da15e1c2cf392
sloria <sloria1@gmail.com>
2014-07-19 16:08:42 -0400

    Add snippets directory

--- vim/install.sh
+++ vim/install.sh
@@ -2,7 +2,16 @@
 
 if [[ ! -a ~/.vim/bundle/Vundle.vim ]]; then
   # Install Vundle
+  echo "Installing Vundle"
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
+
+if [[ ! -a ~/.vim/snippets ]]; then
+  # Link snippets directory
+  echo "Linking snippets directory"
+  ln -s $ZSH/vim/snippets ~/.vim/snippets
+fi
 # Install all plugins
+echo "Installing plugins"
 vim +PluginInstall +qall
+echo "Done"
